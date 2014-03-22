49361d9e80ab995ca690c24e2bb2006de947e2cf
elp87 <elp1987@gmail.com>
2014-03-22 23:49:58 +0400

    Add "tsl2ur.lib.TsLabReader"

---
+++ tsl2ur.Test/TsLabReaderTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tsl2ur.lib;
+
+namespace tsl2ur.Test
+{
+    [TestClass]
+    public class TsLabReaderTest
+    {
+        [TestMethod]
+        public void TestReader()
+        {
+            List<Trade> trades = new List<Trade>();
+            TsLabReader reader = new TsLabReader(@"C:\Users\Elp\Documents\gitHub\TSLab2WebmarketstateConverter\tsl2ur.Test\bin\Debug\TestData\trades.csv");
+            trades = reader.TradeList;
+
+            int a = 1;
+            a++;
+        }
+    }
+}
---
+++ tsl2ur.lib/TsLabReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using elp.Extensions;
+
+namespace tsl2ur.lib
+{
+    public class TsLabReader
+    {
+        #region Fields
+        private List<Trade> _tradeList; 
+        #endregion
+
+        #region Constructors
+        private TsLabReader()
+        {
+            _tradeList = new List<Trade>();
+        }
+
+        public TsLabReader(string fileName)
+            :this()
+        {
+            CSVReader csv = new CSVReader(fileName, _tradeList);
+            csv.hasHeader = true;
+            csv.AddColumn("TsLabTradeType", 0);
+            csv.AddColumn("InstrumentName", 1);
+            csv.AddColumn("Count", 2);
+            csv.AddColumn("EntryReason", 3);
+            csv.AddColumn("TsLabEntryDateTime", 5);
+            csv.AddColumn("EntryPrice", 6);
+            csv.AddColumn("ExitReason", 7);
+            csv.AddColumn("TsLabExitDateTime", 9);
+            csv.AddColumn("ExitPrice", 10);
+            
+            _tradeList = (List<Trade>)csv.finalList;
+        } 
+        #endregion
+
+        #region Properties
+        public List<Trade> TradeList
+        {
+            get { return _tradeList; }
+        } 
+        #endregion
+    }
+}
